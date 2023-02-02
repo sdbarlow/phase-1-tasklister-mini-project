3c6cc8ea74b758cdfb49305158006a5a4f999b88
Seth Barlow <sethdbarlow@gmail.com>
2023-02-01 17:08:28 -0700

    task list working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const frm = document.getElementById('create-task-form');
+  const inpt = document.getElementById("new-task-description");
+  frm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const li = document.createElement('li');
+    document.getElementById('tasks').appendChild(li);
+    li.textContent = inpt.value;
+  })
 });
+
